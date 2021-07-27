9f9196361fb742fc7c17d4b94ecd22b0403141d5
François Caud <82875602+frcaud@users.noreply.github.com>
2021-07-27 11:09:29 +0200

    Update problem.py

--- problem.py
+++ problem.py
@@ -48,7 +48,10 @@ score_types = [
     MAPE(name="MAPE"),
 ]
 
+
 path = "."
+
+
 def get_file_list_from_dir(datadir):
     all_files = os.listdir(os.path.join(path, "data", datadir))
     data_files = list(filter(lambda file: file.endswith('.csv.gz'), all_files))
