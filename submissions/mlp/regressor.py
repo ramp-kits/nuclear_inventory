fix scaling in mlp

submissions/mlp/regressor.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.base import BaseEstimator
 
@@ -13,8 +14,10 @@ class Regressor(BaseEstimator):
         )
 
     def fit(self, X, Y):
-        self.model.fit(X, Y)
+        self.X_scaling_ = np.max(X, axis=0, keepdims=True)
+        self.Y_scaling_ = np.max(Y, axis=0, keepdims=True)
+        self.model.fit(X / self.X_scaling_, Y / self.Y_scaling_)
 
     def predict(self, X):
-        res = self.model.predict(X)
+        res = self.model.predict(X / self.X_scaling_) * self.Y_scaling_
         return res